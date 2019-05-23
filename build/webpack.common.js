1871e05213cfa80c8729ba3eaf180c1b61b437b3
杨肖飞 <xiaofei.yang@tongdun.net>
2019-05-23 15:50:42 +0800

    同步/异步代码分割

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -52,26 +52,26 @@ module.exports = {
       root: path.resolve(__dirname, '../')
     }, 'dist'),
   ],
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-      minSize: 30000,
-      maxSize: 0,
-      minChunks: 1,
-      maxAsyncRequests: 5,
-      maxInitialRequests: 3,
-      automaticNameDelimiter: '~',
-      name: true,
-      cacheGroups: {
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10,
-          filename: 'vendors.js'
-        },
-        default: false
-      }
-    }
-  },
+  // optimization: {
+  //   splitChunks: {
+  //     chunks: 'all',
+  //     minSize: 30000,
+  //     maxSize: 0,
+  //     minChunks: 1,
+  //     maxAsyncRequests: 5,
+  //     maxInitialRequests: 3,
+  //     automaticNameDelimiter: '~',
+  //     name: true,
+  //     cacheGroups: {
+  //       vendors: {
+  //         test: /[\\/]node_modules[\\/]/,
+  //         priority: -10,
+  //         filename: 'vendors.js'
+  //       },
+  //       default: false
+  //     }
+  //   }
+  // },
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, '../dist')
--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-import _ from 'lodash';
+function getComponent() {
+  return import(/* webpackChunkName: 'lodash' */'lodash').then(({ default: _ }) => {
+    const element = document.createElement('div');
+    element.innerHTML = _.join(['a' , 'b', 'c'], '~');
+    return element;
+  });
+}
 
-console.log(_.join(['a', 'b', 'c'], '***'));
+getComponent().then(ele => {
+  document.body.appendChild(ele);
+})
+
+// 代码分割
+
+// 两种方式
+
+// 同步代码分割：只需要在webapck.config.js中optimization配置即可；
+// 异步代码分割：无需配置，webpack打包时会自动进行代码分割
