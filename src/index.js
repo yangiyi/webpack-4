0dd1a5d8c3caed85cffebe475b2afde8d3486591
杨肖飞 <xiaofei.yang@tongdun.net>
2019-06-24 20:14:39 +0800

    code split

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-// Tree Shaking 只支持es module; 底层静态引入，而不支持commonjs，因为commonjs是动态引入模式
+// import _ from 'lodash';
 
-import { add } from './math.js';
+// console.log(_.join(['a', 'b', 'c'], '@@@'));
+// // 此处省略n行业务代码
+// console.log(_.join(['a', 'b', 'c'], '@@@'));
 
-add(1,2);
+// 异步代码分割
+function getComponent() {
+  return import('lodash').then(({default: _}) => {
+    var element = document.createElement('div');
+    element.innerHTML = _.join(['naruto', 'ninja'], '-');
+    return element;
+  })
+}
+getComponent().then((ele) => {
+  document.body.appendChild(ele);
+});
+
+// 代码分割与webpack无关，webpack中实现代码分割，两种方式
+// 1.同步代码，只需要在optimization的配置splitChunks即可
+// 2.异步代码（import）， 无需做任何配置，会自动进行code split
